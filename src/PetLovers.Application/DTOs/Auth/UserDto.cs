ce716f29fdcd36aa9d5c402d9c79db634572bd28
marivaldojunior <marivaldo.pereira@ufba.br>
2026-02-24 20:59:56 -0300

    feat: adiciona fundação de autenticação JWT com refresh token

--- src/PetLovers.Application/DTOs/Auth/UserDto.cs
+++ src/PetLovers.Application/DTOs/Auth/UserDto.cs
@@ -1,5 +1,13 @@
 namespace PetLovers.Application.DTOs.Auth;
-
+/// <summary>
+/// 
+/// </summary>
+/// <param name="Id"></param>
+/// <param name="Email"></param>
+/// <param name="FirstName"></param>
+/// <param name="LastName"></param>
+/// <param name="FullName"></param>
+/// <param name="Roles"></param>
 public sealed record UserDto(
     Guid Id,
     string Email,
